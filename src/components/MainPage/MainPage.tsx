9dd220f402ef24432f8d8f7289246af87de3acd5
MacketSWE <mthorelius@gmail.com>
2024-09-12 14:51:10 +0200

    Add business info

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -36,6 +36,11 @@ const MainPage: React.FC = () => {
     }
   };
 
+  const handleEditBusinessInfo = () => {
+    // TODO: Implement edit functionality
+    console.log("Edit business info clicked");
+  };
+
   return (
     <div
       className={`${styles.appContainer} ${
@@ -52,7 +57,31 @@ const MainPage: React.FC = () => {
 
         <div className={styles.contentWrapper}>
           <h1 className={styles.header}>Monkey Bizz</h1>
-          <div className={styles.businessInfo}>{businessInfo.name}</div>
+          <div
+            className={styles.businessInfoContainer}
+            onClick={handleEditBusinessInfo}
+            role="button"
+            tabIndex={0}
+            aria-label="Edit business info"
+          >
+            <div className={styles.businessInfo}>{businessInfo.name}</div>
+            <div className={styles.editButton}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="lightgray"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                width="16"
+                height="16"
+              >
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+              </svg>
+            </div>
+          </div>
           <CEOCard />
           <div className={styles.roleCardsContainer}>
             {roles.map((role) => (
